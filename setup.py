a62ddad9544cad1d4d080ae5f53e6760cdfed373
Max Sudyin <msudyin@msudyin-mbp20.local>
2021-10-04 23:33:10 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="thermolog",
+    version="0.0.1",
+    author="Max Sudyin",
+    author_email="msudyin@gmail.com",
+    description="Thermolog - Small app to log temperature at my country house",
+    url="https://github.com/makcyd/thermolog",
+    packages=["thermolog"],
+    install_requires=["weback-unofficial", "requests", "Django"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
