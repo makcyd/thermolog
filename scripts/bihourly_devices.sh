930eedb6c7ef8205cd7d58586e77cb486fd80ab1
Max Sudyin <msudyin@gmail.com>
2021-10-05 22:21:10 +0700

    Added dir

--- scripts/bihourly_devices.sh
+++ scripts/bihourly_devices.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+cd /opt/thermolog
 source .venv/bin/activate
 python3 thermolog/manage.py sync_thermometer >> thermolog/logs/sync_thermometer.log 2>&1
--- scripts/bihourly_wather.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source .venv/bin/activate
-python3 thermolog/manage.py sync_weather >> thermolog/logs/sync_weather.log 2>&1
